067547341931dc2f2124eae82840ece9b4dee3a6
inomesh <nomesh.sehgal@gmail.com>
2022-08-24 16:16:51 +0530

    update

--- admin/src/apiClient/Axios.js
+++ admin/src/apiClient/Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: 'http://localhost:80/api',
+  baseURL: 'https://ed13-103-211-13-77.in.ngrok.io/api',
+  // baseURL: 'http://localhost:80/api',
 });
 
 // export const responseInterceptor = Axios.interceptors.response.use((response) => {
--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 const crypto = require("crypto");
-app.use(cors({ origin: true, credentials: true }));
+
+app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
@@ -41,7 +42,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 app.post("/upload", upload.single("file"), (req, res) => {
-  console.log(req.body, "file", req.file);
   res.send({ ...req.file, ...req.body }).end();
 });
 
