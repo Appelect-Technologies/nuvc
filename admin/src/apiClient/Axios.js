1658f9aecb72f2dfa55781f1d8618fecf88988c2
inomesh <nomesh.sehgal@gmail.com>
2022-08-24 14:41:12 +0530

    update

--- admin/src/apiClient/Axios.js
+++ admin/src/apiClient/Axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:80/api'
-      : 'http://pt-load-balancer-2122214972.ap-south-1.elb.amazonaws.com/',
+  baseURL: 'http://localhost:80/api',
 });
 
 // export const responseInterceptor = Axios.interceptors.response.use((response) => {
