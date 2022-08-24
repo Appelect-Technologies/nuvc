72c12720414e7eb89731f4c4c759a9f312ec4e51
inomesh <nomesh.sehgal@gmail.com>
2022-08-24 13:51:08 +0530

    updated client gitignore

---
+++ client/src/config.js
@@ -0,0 +1,22 @@
+export const config = {
+  // baseUrl: "https://nuvc.org", //"https://seedonline.org","http://localhost:4000"
+  baseUrl: "http://localhost:80",
+  register: "/api/reg",
+  personal: "/api/personal",
+  address: "/api/address",
+  qulification: "/api/qulification",
+  prefrance: "/api/prefrance",
+  document: "/api/document",
+  upload: "/upload",
+  pay: "/pay10",
+  subscription: "/api/subscription",
+  job: "/api/job",
+  jobs: "/api/jobs",
+  jobsByEmail: "/api/getJobsByEmail",
+  digital: "/api/digital",
+  digitals: "/api/digitals",
+  checkApplyStatus: "/api/checkApplyStatus",
+  news: "/api/news",
+  getAllNews: "/api/getAllNews",
+  leads: "/api/leads",
+};
