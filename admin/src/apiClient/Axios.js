ed1ed62f458fb3fe6d29567f2e63310a1db8d301
inomesh <nomesh.sehgal@gmail.com>
2022-08-27 05:23:57 +0530

    updated axios baseurl in admin

--- admin/src/apiClient/Axios.js
+++ admin/src/apiClient/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:80' : 'http://api.nuvc.org',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:80' : 'https://api.nuvc.org',
 });
 
 // export const responseInterceptor = Axios.interceptors.response.use((response) => {
