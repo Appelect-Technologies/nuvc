5ce7cad6ff5a8d09a371945ed5e189daa19b9857
vohraratik <99384338+vohraratik@users.noreply.github.com>
2022-09-16 15:47:58 +0530

    Update Nav.js

--- client/src/components/header/Nav.js
+++ client/src/components/header/Nav.js
@@ -96,11 +96,11 @@ function Nav() {
                 </ul>
               </div>
             </li>
-            <li className="nav-item">
-              <Link to="/trainingCentre" className="nav-link" href="#">
-              Training Centre
-              </Link>
-            </li>
+//             <li className="nav-item">
+//               <Link to="/trainingCentre" className="nav-link" href="#">
+//               Training Centre
+//               </Link>
+//             </li>
 
             <li className="nav-item">
               <Link to="/contact" className="nav-link" href="#">
