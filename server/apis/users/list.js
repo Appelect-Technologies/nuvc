280c26af59a80a6715674eb4314836d7446773e1
inomesh <nomesh.sehgal@gmail.com>
2022-10-17 14:24:07 +0530

    removed 1000 records limit from get users api in server

--- server/apis/users/list.js
+++ server/apis/users/list.js
@@ -2,12 +2,31 @@ const { getAuth } = require("firebase-admin/auth");
 var SibApiV3Sdk = require("sib-api-v3-sdk");
 var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
+let users = [];
+
 const getUsers = async (req, res) => {
   try {
-    const lists = await getAuth(global.firebaseApp).listUsers();
-    const users = lists.users.map((user) => user.providerData).flat();
-    return res.send({ msg: "success", users });
-  } catch (error) {}
+    await getAllUsers();
+    const filtereddata = users.flat().map((_) => _.providerData);
+    return res.send({ msg: "success", users: filtereddata.length });
+  } catch (error) {
+    return res.status(400).json({ msg: "error", error: error.message });
+  } finally {
+    users = [];
+  }
 };
+
+async function getAllUsers(pageToken) {
+  let results = {};
+  if (pageToken) {
+    results = await getAuth(global.firebaseApp).listUsers(1000, pageToken);
+  } else {
+    results = await getAuth(global.firebaseApp).listUsers(1000);
+  }
+  users.push(results.users);
+  if (results.pageToken) {
+    await getAllUsers(results.pageToken);
+  }
+}
 
 module.exports = { getUsers };
