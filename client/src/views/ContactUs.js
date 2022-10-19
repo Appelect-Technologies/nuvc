9aacf7fa54f670113c58d6c5fb52ff059d8e4754
vohraratik <99384338+vohraratik@users.noreply.github.com>
2022-10-19 11:00:20 +0530

    updated address

--- client/src/views/ContactUs.js
+++ client/src/views/ContactUs.js
@@ -67,7 +67,6 @@ function ContactUs() {
                   <b>Address:</b> B15 Shankar Garden, Janakpuri west metro station, New
                   Delhi-110018
                   <br></br>
-                  <b>Registered Office:</b> D9 Rajdhani Park New Delhi 110041
                 </address>
                 <div class="emailfield">
                   <p>
