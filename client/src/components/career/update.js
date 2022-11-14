e0f2393a86d5d9e0ba839066dba13f51fd3c4f84
Gauravshrivastav <gauravshrivastav952@gmail.com>
2022-11-14 15:13:25 +0530

    udate

--- client/src/components/career/update.js
+++ client/src/components/career/update.js
@@ -6,7 +6,7 @@ function update() {
     <>
       <div className="update" style={{ height: "70vw" }}>
         <img
-          src="https://nuvc-public.s3.ap-south-1.amazonaws.com/bcd.jpeg"
+          src="https://nuvc-public.s3.ap-south-1.amazonaws.com/WhatsApp+Image+2022-11-14+at+2.58.33+PM.jpeg"
           alt="/"
           style={{ width: "80vw", marginLeft: "10%", marginTop: "5%" }}
         />
